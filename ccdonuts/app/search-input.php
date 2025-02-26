fa7e33ac90ed5c497d0c4d3287bfb086b44462a5
runta-001 <k.h.9.2.8rh3.cca@gmail.com>
2025-02-26 13:33:59 +0900

    Delete ccdonuts directory

--- ccdonuts/app/search-input.php
+++
@@ -1,6 +0,0 @@
-<?php require '../header.php'; ?>
-<form action="search-output.php" method="post">
-    <input type="text" name="keyword">
-    <input type="submit" value="検索">
-</form>
-<?php require '../footer.php'; ?>
--- ccdonuts/app/search-output.php
+++
@@ -1,7 +0,0 @@
-<?php require '../header.php'; ?>
-<table>
-    
-<!-- 参考 P224  -->
-
-</table>
-<?php require '../footer.php'; ?>
